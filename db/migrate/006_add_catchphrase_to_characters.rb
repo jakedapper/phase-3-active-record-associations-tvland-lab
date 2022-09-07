653b210a6460d1e15b9e4efd6b6c932876f95071
jake dapper <jakedapper@gmail.com>
2022-09-06 18:28:40 -0600

    commit tree 9331b85a6d61e70201e2ee1eb42cf2ea797a3f63

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,8 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :catchphrases do |t|
+      t.string :catchprase
+    end
   end
 end
